cf241903a719d04710d62f793eb148c7799f08e4
timteal <timothy.teal1@gmail.com>
2023-09-25 20:34:13 -0600

    refactored with clearer function names

--- koradPS.py
+++ koradPS.py
@@ -24,58 +24,52 @@ class KoradPS:
 		self.ser = serial.Serial(port=port, baudrate=9600, bytesize=8, parity='N', stopbits=1, timeout=1)
 		self.responseTime = 0.050 # per the manual response time is 50ms. 
 
-	def iset(self, amps=None, channel=1):
 	# iset() sets the current limit, or returns the limit setting if no "amps" value is passed
 	# ISET<X>:<parameter> - Set Current Limit, example ISET1:3.5 sets the channel 1 current limit to 3.5A
 	# ISET<X>? - Return Current Limit setting
-		if amps != None:
-			cmd = 'ISET{}:{}'.format(channel, amps)
-			self.send(cmd)
-		else:
-			cmd = "ISET{}?".format(channel)
-			return self.read(cmd)
+	# IOUT<X>? - Return actual output current
+	def read_current_limit(self, channel=1):
+		cmd = "ISET{}?".format(channel)
+		return self.read(cmd)
+	
+	def set_current_limit(self, amps, channel=1):
+		cmd = 'ISET{}:{}'.format(channel, amps)
+		self.send(cmd)
 
+	def read_current_output(self, channel=1):
 
-	def vset(self, volts=None, channel=1):
+		cmd = "IOUT{}?".format(channel)
+		return self.read(cmd)
+
 	# vset() sets the voltage limit, or returns the limit setting if no "volts" value is passed
 	# VSET<X>:<parameter> - Set Voltage Limit
 	# VSET<X>? - Return Voltage Limit setting
-		if volts != None:
-			cmd = 'VSET{}:{}'.format(channel, volts)
-			self.send(cmd)
-		else:
-			cmd = "VSET{}?".format(channel)
-			return self.read(cmd)
-
-
-	def iout(self, channel=1):
-	# IOUT<X>? - Return actual output current
-		cmd = "IOUT{}?".format(channel)
-		return self.read(cmd)		
-
-
-	def vout(self, channel=1):
 	# VOUT<X>? - Return actual output voltage
+	def read_voltage_limit(self, channel=1):
+		cmd = "VSET{}?".format(channel)
+		return self.read(cmd)
+	
+	def set_voltage_limit(self, volts, channel=1):
+		cmd = 'VSET{}:{}'.format(channel, volts)
+		self.send(cmd)
+
+	def read_voltage_output(self, channel=1):
 		cmd = "VOUT{}?".format(channel)
-		return self.read(cmd)	
+		return self.read(cmd)
 
-
-	def on(self):
 	# OUT<Y> - Turns output on or off. 1=On, 0=Off	
+	def output_on(self):
 		cmd = "OUT1"
 		self.send(cmd)
 
-	def off(self):
-	# OUT<Y> - Turns output on or off. 1=On, 0=Off	
+	def output_off(self):
 		cmd = "OUT0"
 		self.send(cmd)
 
-
-	def status(self):
 	# STATUS? - Returns the power supply status as two bytes.
 	# bit 0 is the CC/CV mode
 	# bit 8 is the output on/off (I think. I'm guessing a bit because I think the manual is wrong.)
-
+	def get_status(self):
 		cmd = "STATUS?"
 		self.ser.write(cmd.encode())
 		response = self.ser.read(2)
@@ -96,37 +90,35 @@ class KoradPS:
 
 		return ("Mode: {}, Output: {}".format(mode,output))
 
-	def id(self):
 	# *IDN? - Returns the device identification
+	def get_id(self):
 		cmd = "*IDN?"
 		self.ser.write(cmd.encode())
 		response = self.ser.readline().decode()
 		return response	
 
-	def recall(self, setting=1):
 	# RCL<X> - Recalls a panel setting, where X is a number 1-5. Example: RCL2 recalls panel setting 2
+	# SAV<X> - Stores the panel settings in memory number X
 	# Both RCL and SAV turn the power suplies output OFF, I have intentionally not changed that behavior
+	def recall_settings(self, setting):
 		cmd = "RCL{}".format(setting)
 		self.send(cmd)
 
-	def save(self, setting=1):
-	# SAV<X> - Stores the panel settings in memory number 1
-	# Both RCL and SAV turn the power suplies output OFF, I have intentionally not changed that behavior
+	def save_settings(self, setting):
 		cmd = "SAV{}".format(setting)
 		self.send(cmd)
 
-	def ocp(self, setting=1):
 	# OCP<X> - Over current. OCP1 sets OCP on, OCP0 to off, defaults to on
+	def ocp(self, setting=1):
 		cmd = "OCP{}".format(setting)
 		self.send(cmd)
 
-	def send(self,cmd):
 	# send() and read() do the actual serial communication for most of the other functions
-			self.ser.write(cmd.encode())
-			time.sleep(self.responseTime)
+	def send(self,cmd):
+		self.ser.write(cmd.encode())
+		time.sleep(self.responseTime)
 
 	def read(self,cmd):
-	# send() and read() do the actual serial communication for most of the other functions
 		self.ser.write(cmd.encode())
 		response = self.ser.read(5)
 		return float(response.decode())
